04601663cf40f4d8d820ba2bc27ec06f1120fc50
Khalil <kadrikhalil@gmail.com>
2023-10-15 11:13:56 +0530

    Initial Commit

---
+++ SmartFM/SmartFMApp.swift
@@ -0,0 +1,17 @@
+//
+//  SmartFMApp.swift
+//  SmartFM
+//
+//  Created by I Smart Facitech PVT LTD on 15/10/23.
+//
+
+import SwiftUI
+
+@main
+struct SmartFMApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
