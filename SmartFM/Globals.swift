bbaf90316970c852ee4d96d56d9a1fab520ca00a
Khalil <kadrikhalil@gmail.com>
2023-10-15 11:58:05 +0530

    Created globals class

---
+++ SmartFM/Globals.swift
@@ -0,0 +1,104 @@
+//
+//  Globals.swift
+//  SmartFM
+//
+//  Created by I Smart Facitech PVT LTD on 15/10/23.
+//
+
+import Foundation
+class Globals {
+    public static let BASE_URL = "https://smartfm.ismartfacitechpl.com/"
+    public static let API_URL = BASE_URL + "android.php"
+    
+    // user data
+    public static let USER_ID_KEY = "USER_ID_KEY"
+    public static let USER_TOKEN_KEY = "USER_TOKEN_KEY"
+    public static let USER_NAME_KEY = "USER_NAME_KEY"
+    public static let CHECK_IN_OUT_KEY = "CHECK_IN_OUT_KEY"
+    public static let IS_LOGGED_IN_KEY = "IS_LOGGED_IN_KEY"
+    public static let IS_APPROVED_KEY = "IS_APPROVED_KEY"
+    public static let IS_REPORTING_MANAGER_KEY = "IS_REPORTING_MANAGER_KEY"
+    
+    // main views
+    public static let LOGIN_VIEW = "LOGIN_VIEW"
+    public static let DASHBOARD_VIEW = "DASHBOARD_VIEW"
+    public static let WEBUI_VIEW = "WEBUI_VIEW"
+    public static let ATTENDANCE_VIEW = "ATTENDANCE_VIEW"
+    public static let CURRENT_VIEW = LOGIN_VIEW
+    
+    
+    public static func saveData(key: String, value: String) {
+        UserDefaults.standard.setValue(value, forKey: key)
+    }
+    
+    public static func getData(key: String) -> String {
+        return UserDefaults.standard.string(forKey: key) ?? ""
+    }
+    
+    public static func getUserId() -> String {
+        return getData(key:USER_ID_KEY)
+    }
+    
+    public static func getUserToken() -> String {
+        return getData(key:USER_TOKEN_KEY)
+    }
+    
+    public static func getUserName() -> String {
+        return getData(key:USER_NAME_KEY)
+    }
+    
+    public static func getCheckInStatus() -> String {
+        return getData(key:CHECK_IN_OUT_KEY)
+    }
+    
+    public static func getLoggedInStatus() -> String {
+        return getData(key:IS_LOGGED_IN_KEY)
+    }
+    
+    public static func getIsApproved() -> String {
+        return getData(key:IS_APPROVED_KEY)
+    }
+    
+    public static func getIsReportingManager() -> String {
+        return getData(key:IS_REPORTING_MANAGER_KEY)
+    }
+    
+    public static func getWebViewURL(page: String) -> String {
+        let token = getUserToken()
+        let url = BASE_URL + page + "?user_token=" + token
+        return url
+    }
+
+    public static func isLoggedIn() -> Bool {
+        let status = getLoggedInStatus()
+        
+        if status.isEmpty {
+            return false
+        }
+        
+        if status == "0" {
+            return false
+        }
+        
+        if status == "1" {
+            return true
+        }
+            
+        return false
+    }
+    
+    public func isCheckedIn() -> Bool {
+        let status = Globals.getCheckInStatus()
+        
+        if status.isEmpty {
+            return false;
+        }
+        
+        if status == "1" {
+            return true
+        }
+        
+        return false
+    }
+
+}
